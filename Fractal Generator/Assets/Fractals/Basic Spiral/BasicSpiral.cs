61dfb8a9588bc4e1476a62f8d1cf8323eabc7d60
StudentEnder <89277510+StudentEnder@users.noreply.github.com>
2021-10-08 15:04:55 -0600

    created Basic Spiral prefab with simple instantiator

--- Fractal Generator/Assets/Fractals/Basic Spiral/BasicSpiral.cs
+++ Fractal Generator/Assets/Fractals/Basic Spiral/BasicSpiral.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicFractal : MonoBehaviour
+public class BasicSpiral : MonoBehaviour
 {
+    public Quaternion rotation; 
+    public Transform topPoint;
+    public Transform bottomPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 top = topPoint.position;
+        Vector3 bottom = bottomPoint.position;
+
+        Instantiate(gameObject, top, rotation, transform);
+
+        Debug.Log("created first component of spiral");
     }
 
     // Update is called once per frame
