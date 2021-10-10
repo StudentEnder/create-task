32158dde3b8014b3c7084308852f95dc63143c3a
StudentKyle <studentkyles@gmail.com>
2021-10-09 19:39:00 -0600

    Adds an abstract RecursiveObjectFractal monobehaviour for handling basic features for fractals. It handles depth counting and gameObject inheritance and supports custom object preparation and children creation.

---
+++ Fractal Generator/Assets/Fractals/Generic/RecursiveObjectFractal.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class RecursiveObjectFractal : MonoBehaviour
+{   
+    [Header("Recursive Object Fractal")]
+    [Tooltip("Current Depth")]
+    [SerializeField]
+    protected int depth; // current depth
+    [Tooltip("Depth distance from final object spawn. This decrements until 0, as the final depth spawn.")]
+    [SerializeField]
+    protected int depthCountdown;
+
+    //private bool initialized = false;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        if (depthCountdown > 0) // don't make more children if depthCountdown of 0 is reached.
+        {
+            CreateChildren();
+        }
+    }
+
+    /// <summary>
+    /// Initializes fractal spawn information. This must be called before the object can create children.
+    /// </summary>
+    /// <param name="parent"></param>
+    public void Initialize(RecursiveObjectFractal parent)
+    {
+        transform.parent = parent.transform; // sets the gameObject hierarchy to make children the children.
+        depth = parent.depth + 1; // increases recursion depth value
+        depthCountdown = parent.depthCountdown - 1; // decrements depthCountdown from the parent's value.
+        Prepare(parent);
+        //initialized = true;
+    }
+
+    /// <summary>
+    /// Fractal-specific initialization behavior. Called in <see cref="Initialize(RecursiveObjectFractal)"/>.
+    /// </summary>
+    /// <param name="parent"></param>
+    public abstract void Prepare(RecursiveObjectFractal parent);
+
+    /// <summary>
+    /// Fractal-specific children creation behavior. Called in <see cref="Start"/> as <see cref="depthCountdown"/> is over 0. <see cref="Initialize(RecursiveObjectFractal)"/> must first be ran.
+    /// </summary>
+    public abstract void CreateChildren();
+
+}
