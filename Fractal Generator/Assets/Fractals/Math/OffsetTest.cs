d72a73e33098d6731cbb8207ef0e6bc78da1dd05
StudentKyle <studentkyles@gmail.com>
2021-10-29 15:18:54 -0600

    Cleans up OffsetTest

--- Fractal Generator/Assets/Fractals/Math/OffsetTest.cs
+++ Fractal Generator/Assets/Fractals/Math/OffsetTest.cs
@@ -26,7 +26,9 @@ public class OffsetTest : MonoBehaviour
     public Quaternion rotationAmount;
 
 
+
     public delegate float DepthFunction(int depth);
+
 
 
     // Start is called before the first frame update
@@ -66,19 +68,6 @@ public class OffsetTest : MonoBehaviour
         float scale = Mathf.Pow(scalar, currentDepth);
         return scale;
     }
-
-    /*  Old Offset function, optimized in new function
-    public float Offset(int currentDepth)
-    {
-        float offset = 0f;
-        for (int depth = 0; depth < currentDepth; depth++)
-        {
-            offset += Scale(depth + 1);
-        } 
-
-        // Final offset to account for placement being from the center of the object.
-        return offset +.5f - (.5f * Scale(currentDepth));
-    } */
     
     /// <summary>
     /// Returns centered position of object at specified depth.
@@ -95,14 +84,13 @@ public class OffsetTest : MonoBehaviour
 
     public float Summation(int start, int max, DepthFunction f)
     {
-        float offset = 0f;
-        for (int depth = 0; depth < max; depth++)
+        float sum = 0f;
+        for (int depth = start; depth < max; depth++)
         {
-            offset += f(depth);
+            sum += f(depth);
         }
 
-
-        return offset;
+        return sum;
     }
 
     /// <summary>
@@ -112,7 +100,6 @@ public class OffsetTest : MonoBehaviour
     /// <returns></returns>
     public Quaternion Rotate(float currentDepth)
     {
-
         return Quaternion.identity;
     }
 }
