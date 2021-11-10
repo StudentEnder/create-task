a6332defe14f7e33ea92016b6403882c22903d8e
StudentKyle <studentkyles@gmail.com>
2021-11-10 14:45:50 -0700

    Adds comments explaining the unintuitive situation of the cylinder along the y-axis

--- Fractal Generator/Assets/Fractals/Line Fractal/LengthCapsule.cs
+++ Fractal Generator/Assets/Fractals/Line Fractal/LengthCapsule.cs
@@ -10,7 +10,9 @@ public class LengthCapsule : MonoBehaviour
 
     public void SetLength(float length)
     {
+        
         length *= .5f;
+        // cylinder y axis changes because cylinder object starts vertical, but we rotated it horizontal in prefab.
         cylinder.localScale = new Vector3(cylinder.localScale.x, length, cylinder.localScale.z);
 
         topCap.localPosition = new Vector3(length, topCap.localPosition.y, topCap.localPosition.z);
@@ -19,6 +21,7 @@ public class LengthCapsule : MonoBehaviour
 
     public void SetRadius(float radius)
     {
+        // cylinder y axis constant because y axis is parallel to cylinder direction
         cylinder.localScale = new Vector3(radius, cylinder.localScale.y, radius);
 
         topCap.localScale = new Vector3(radius, radius, radius);
