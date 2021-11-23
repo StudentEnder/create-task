47ca342a5b150665a4bee1f386043b6518eae94a
StonebayKyle <stonebay.kyle@gmail.com>
2021-11-22 19:03:40 -0700

    Makes Main Camera be a gameobject child of the Manual Player to attach the camera to the player, and locks the cursor. Horizontal constraints aren't intuitive at all as one can rotate their forward perspective without tilt. TODO: fix this.

--- Fractal Generator/Assets/Player/Manual Controller/ManualFreeCam.cs
+++ Fractal Generator/Assets/Player/Manual Controller/ManualFreeCam.cs
@@ -21,7 +21,7 @@ public class ManualFreeCam : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
