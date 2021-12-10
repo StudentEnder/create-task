1ecc7f6a48ebce6d37fa0be4e666f0314ad15627
Ender <89277510+StudentEnder@users.noreply.github.com>
2021-12-10 14:57:35 -0700

    WIP Starts UI
    TODO button cannot be interacted with

---
+++ Fractal Generator/Assets/logAction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logAction : MonoBehaviour
+{
+    public void printLogAction()
+    {
+        Debug.Log("action");
+    }
+}
